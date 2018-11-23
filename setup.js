d0bbc617390ce902fce860ffb945d568322a7d3b
Vladimir Grinenko <i@tadatuta.com>
2018-11-24 00:53:19 +0300

    initial

---
+++ setup.js
@@ -0,0 +1,7 @@
+import XTable from '../contribs/lego-on-react/es-modules-src/components/x-table/x-table.react.js';
+import XArea from '../contribs/lego-on-react/es-modules-src/components/x-area/x-area.react.js';
+import XTitle from '../contribs/lego-on-react/es-modules-src/components/x-title/x-title.react.js';
+
+global.XTable = XTable;
+global.XArea = XArea;
+global.XTitle = XTitle;
